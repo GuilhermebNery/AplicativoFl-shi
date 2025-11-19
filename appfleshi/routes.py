d9cb49920f22f9eac1ab6469b9e32799e487da4c
AlunoSenai <guilherme.b.nery6@aluno.senai.br>
2025-11-19 14:48:29 -0300

    feat: login and security system setup

--- appfleshi/routes.py
+++ appfleshi/routes.py
@@ -1,4 +1,6 @@
 from flask import render_template
+from flask_login import login_required
+
 from appfleshi import app
 
 
@@ -6,6 +8,7 @@ from appfleshi import app
 def homepage():
     return render_template('homepage.html')
 
-@app.route('/profile/<username>', methods=['GET'])
+@app.route('/profile/<username>')
+@login_required
 def profile(username):
     return render_template('profile.html', username=username)
