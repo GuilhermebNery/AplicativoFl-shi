c3fc00ea24da471b6339dbc9c4dd108f049f843c
Aluno <guilherme.b.nery6@aluno.senai.br>
2025-12-03 15:35:40 -0300

    feat: add the feature to post a photo

--- appfleshi/forms.py
+++ appfleshi/forms.py
@@ -1,8 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, FileField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError, Length
 from appfleshi.models import User
 
+
+
+class PhotoForm(FlaskForm):
+    photo = FileField('Foto', validators=[DataRequired()])
+    submit = SubmitField('Postar')
 
 
 class LoginForm(FlaskForm):
